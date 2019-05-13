4cd402e9b4185a947cadbdb0cd8becf585f47120
Kasun Kodagoda <kvkrusl@gmail.com>
2019-05-13 11:08:28 +0530

    Logger implementation added

---
+++ OpenGenericExample/OpenGenericExample.Web/Logging/ILogWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenGenericExample.Web.Logging
+{
+    public interface ILogWriter<T>
+    {
+        void LogInfo(string message);
+        void LogWarning(string message, Exception ex = null);
+        void LogError(string message, Exception ex);
+        void LogEvent(string evt, IDictionary<string, object> metrics);
+    }
+}
---
+++ OpenGenericExample/OpenGenericExample.Web/Logging/TraceLogWriter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace OpenGenericExample.Web.Logging
+{
+    public class TraceLogWriter<T> : ILogWriter<T>
+    {
+        private readonly string _messagePrefix;
+
+        public TraceLogWriter()
+        {
+            _messagePrefix = $"[{typeof(T).Name}] ";
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            Trace.TraceError($"{_messagePrefix}{message} Error: {ex.Message}");
+        }        
+
+        public void LogInfo(string message)
+        {
+            Trace.TraceInformation($"{_messagePrefix}{message}");
+        }
+
+        public void LogWarning(string message, Exception ex = null)
+        {
+            var sb = new StringBuilder();
+            sb.Append($"{_messagePrefix}{message}");
+
+            if (ex != null)
+            {
+                sb.Append($" Error: {ex.Message}");
+            }
+
+            Trace.TraceError(sb.ToString());
+        }
+        public void LogEvent(string evt, IDictionary<string, object> metrics)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
